ac31acc784d108edaef94ad716c772b425ff1ff5
Matheus Marques <134228030+mqrques@users.noreply.github.com>
2023-11-28 15:26:08 -0300

    Changes

---
+++ js/conex.js
@@ -0,0 +1,5 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = "https://lanch-etec.vercel.app/options.html";
+} else {
+    window.location.href = "URL_para_desktop";
+}
